ca4bc1c539f9a583f1b2f236da8200df6107c397
Amr Mohamed <punisher297@gmail.com>
2025-09-15 00:51:15 +0200

    .

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,44 +1,30 @@
 from django.db import models
-from django.contrib.auth.models import User
+
 # Create your models here.
+
 class Author(models.Model):
-    name = models.CharField(max_length=255)
-    
+    name = models.CharField(max_length=100)
+
     def __str__(self):
         return self.name
 
 class Book(models.Model):
-    title = models.CharField(max_length=255)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
-    class Meta:
-        permissions = [
-            ("can_add_book", "can_add_book"),
-            ("can_change_book", "can_change_book"),
-            ("can_delete_book", "can_delete_book"),
-        ]
+    title = models.CharField(max_length=150)
+    author = models.ForeignKey(Author,on_delete=models.CASCADE)
+
     def __str__(self):
-        return self.title
+        return self.name
 
 class Library(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=150)
     books = models.ManyToManyField(Book)
+
     def __str__(self):
         return self.name
 
 class Librarian(models.Model):
-    name = models.CharField(max_length=255)
-    library = models.OneToOneField(Library, on_delete=models.CASCADE)
+    name = models.CharField(max_length=150)
+    library = models.OneToOneField(Library,on_delete=models.CASCADE)
+
     def __str__(self):
         return self.name
-
-class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    ROLE_CHOICES = [
-        ('Admin', 'Admin'),
-        ('Librarian', 'Librarian'),
-        ('Member', 'Member')
-    ]
-    role = models.CharField(max_length=255, choices=ROLE_CHOICES)
-
-def create_user_profile(instance):
-    UserProfile.objects.create(user=instance)
--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,22 +1,17 @@
-from relationship_app.models import Book, Author, Library, Librarian
+from .models import Book,Author,Librarian,Library
+from django.shortcuts import get_object_or_404
 
-def get_book_by_author(author_name):
-    # 1. Query all books by a specific author
-    author = Author.objects.get(name=author_name)
-    books = Book.objects.filter(author=author) 
+def list_all_books(request,author_name):
+    author = Author.objects.get(name = author_name)
+    books = Book.objects.filter(author = author)
     return books
 
-    # 2. List all books in a library
-def get_all_books_by_library(library_name):
-    library = Library.objects.get(name=library_name)
-    books= library.books.all()
-    return books
+def books_in_library(request,library_name):
+    library = get_object_or_404(Library,name = library_name)
+    books_in_library = library.books.all()
+    return books_in_library
 
-    # 3. Retrieve the librarian for a library
-def get_librarian_of_library(library_name):
-     library= Library.objects.get(name=library_name)
-     librarian = Librarian.objects.get(library=library)
-     return librarian
-   
-      
+def get_librarian(request,library_name):
+    library = get_object_or_404(Library,name = library_name)
+    librarians = library.librarian
     
