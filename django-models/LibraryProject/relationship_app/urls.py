794f63bb1c7e5966e4e27166035402a245dbec6c
Amr Mohamed <punisher297@gmail.com>
2025-09-14 21:16:45 +0200

    .

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import list_books
 from .views import LibraryDetailView
 urlpatterns = [
-    path('', list_books, name='books'),
-    path('library/',LibraryDetailView.as_view(),name='library-detail')
+    path('books/', list_books, name='books'),
+    path('library/<int:pk>/',LibraryDetailView.as_view(),name='library_detail')
 ]
