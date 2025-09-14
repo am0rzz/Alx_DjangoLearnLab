1286d1b0efd16bd57ca2647027b768db6fcd9036
Amr Mohamed <punisher297@gmail.com>
2025-09-14 21:07:41 +0200

    .

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -11,6 +11,7 @@ class Author(models.Model):
 class Book(models.Model):
     title = models.CharField(max_length=150)
     author = models.ForeignKey(Author,on_delete=models.CASCADE)
+    
     def __str__(self):
         return self.title
 
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
+from .models import Author, Book, Library, Librarian
 
 # Create your views here.
+
+def list_books(request):
+    books = Book.objects.all()
+    context = {'book title': books}
+    return render(request,'relationship_app/list_books.html',context)
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = "library"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['books'] = self.object.books.all()
+        return object
+
+
+
