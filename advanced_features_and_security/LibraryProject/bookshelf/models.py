3c34aac3e05de4d7f1c1e733e304b4d4bbfcd5fb
Amr Mohamed <punisher297@gmail.com>
2025-09-16 00:01:55 +0200

    .

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth import get_user_model
 from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import Group, Permission
 
 
 # Create your models here.
@@ -10,6 +11,14 @@ class Book(models.Model):
     title = models.CharField(max_length =200)
     author = models.CharField(max_length= 100)
     publication_year = models.IntegerField()
+
+    class Meta:
+        permessions = [
+            ('can_view', 'Views Books'),
+            ('can_create','Add Books'),
+            ('can_edit','Edit Books'),
+            ('can_delete','Delete Books')
+        ]
 
 # Create a Custom User.
 class CustomUser(AbstractUser):
@@ -30,3 +39,7 @@ class CustomUserManager(BaseUserManager):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
         return self.create_user(date_of_birth, photo, password, **extra_fields)
+    
+Editors, created = Group.objects.get_or_create(name='can_edit')
+Viewers, created = Group.objects.get_or_create(name='can_view')
+Admins, created = Group.objects.get_or_create(name='can_delete')
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.decorators import permission_required
+
 
 # Create your views here.
+
+
+def add_book(request):
+    return HttpResponse('Added')
+
+@permission_required('bookshelf.can_edit',raise_exception=True)
+def edit_book(request):
+    return HttpResponse('Edit')
+
+
+def delete_book(request):
+    return HttpResponse('Delete')
+
