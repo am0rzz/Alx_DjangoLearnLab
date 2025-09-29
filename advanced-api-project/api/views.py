7f1bc5c9434b3f343fff1a4d2b84d29212335397
Amr Mohamed <punisher297@gmail.com>
2025-09-29 05:59:07 +0200

    .

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -4,7 +4,7 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticate
 from .models import Book
 from .serializers import BookSerializer
 from django_filters.rest_framework import DjangoFilterBackend
-
+from django_filters import rest_framework
 
 # Create your views here.
 class BookListView(generics.ListAPIView):
